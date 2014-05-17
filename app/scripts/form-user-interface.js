5a441428cc44047edff6ad21e8a26454042b7ca9
materliu <materliu@gmail.com>
2014-05-17 21:17:01 +0800

    add uncomplete code !

--- app/scripts/form-user-interface.js
+++ app/scripts/form-user-interface.js
@@ -7,10 +7,38 @@
 (function() {
     'use strict';
 
+    var UTILS = {
+        /**
+         * 获取 e 元素的第n个父节点
+         * @param e
+         * @param n
+         */
+        parent: function (e, n) {
+
+            if (n === undefined) {
+                return n;
+            }
+
+            while(n-- && e) {
+                e = e.parentNode();
+            }
+
+            if (!e || e.nodeType !== 1) {
+                return null;
+            }
+
+            return e;
+        }
+    };
+
     function init() {
         var formOrder = document.forms.order,
             saveButton = document.getElementById('save-button'),
+            qtyFields = formOrder.quantity,
+            qtyLength = qtyFields.length,    // 这里有一个点需要注意， 那就是如果命中的input元素只有一个的话，length的值将为undefined
             saveButtonClicked;
+
+        debugger;
 
         function saveForm() {
 
@@ -23,6 +51,73 @@
 
         saveButton.addEventListener('click', saveForm, false);
 
+        /**
+         * 计算当前货品对应的总价格
+         */
+        function cacluteTotal(e) {
+
+            var inputTarget;    // 被用户点击的input[type=number]元素
+
+            /*
+             * 如果是通过事件触发的， 只修改当前行对应的商品的价值
+             * 如果是自执行， 修改所有商品对应的价值
+             */
+
+            /**
+             * 计算单行商品的价值
+             * @param trElement 单行商品呈现所在的tr html 元素
+             * @private
+             */
+            function _cacluteSingleGoodTotalPrice(trElement) {
+                debugger;
+                var goodQunatityElement = trElement.querySelector('[name="quantity"]'),
+                    goodTotalPriceElement = trElement.querySelector('.price-total');
+
+            }
+
+            if (e) {
+
+                // 传入的e对象的target为input[type=number]元素
+                inputTarget = e.target;     // 在IE8及之前需要使用srcElement
+
+                // 获取inputTarget对应的trElement
+                _cacluteSingleGoodTotalPrice(UTILS.parent(inputTarget, 2));
+
+            } else {
+
+                // 计算所有商品的价格
+                for (var i=0; i<qtyLength; i++) {
+                    _cacluteSingleGoodTotalPrice(UTILS.parent(qtyFields[i]), 2);
+                }
+            }
+        }
+
+        cacluteTotal();
+
+        /**
+         * 只执行一次的函数， 用来在quantity上绑定对应的监听函数来响应用户对货品数量选择的变更
+         * 对应修改货品的总价值
+         */
+        (function listenOrderChange() {
+
+            for (var i= 0; i<qtyLength; i++) {
+                qtyFields[i].addEventListener('input', cacluteTotal, false);
+                qtyFields[i].addEventListener('keyup', cacluteTotal, false);
+            }
+
+        })();
+
+
+
+
+    }
+
+    /**
+     * 将一个数值，转换成钱的表示方式， 类似于 ￥***,***这种形式
+     * @param value
+     */
+    function formatMoney(value) {
+        return '￥' + value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
     }
 
     window.addEventListener('load', init, false);
